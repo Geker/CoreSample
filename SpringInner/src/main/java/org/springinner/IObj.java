2faf91b9d7cf1d3f818bb792ce6a3c3c0d9eb455
songqingqing <songqingqing@foxmail.com>
2017-04-13 16:06:21 +0800

    增加IObj的注释

--- SpringInner/src/main/java/org/springinner/IObj.java
+++ SpringInner/src/main/java/org/springinner/IObj.java
@@ -1,4 +1,20 @@
 package org.springinner;
+
+/**
+ * <p>
+ * Title: IObj
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Copyright: ufo Copyright (C) 2017
+ * </p>
+ *
+ * @author SONGQQ
+ * @version
+ * @since 2017年4月13日
+ */
 public interface IObj {
 
 }
