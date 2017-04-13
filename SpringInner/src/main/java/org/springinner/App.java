8dcebd59c301f02cd9c5f2e39c567e5ba94e40be
songqingqing <songqingqing@foxmail.com>
2017-04-13 17:10:23 +0800

    commit

--- SpringInner/src/main/java/org/springinner/App.java
+++ SpringInner/src/main/java/org/springinner/App.java
@@ -12,9 +12,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        // Resource resource = new ClassPathResource("applicationContext.xml");
+        // 只能读取main/resources下的文件。
+        @SuppressWarnings("resource")
         ClassPathXmlApplicationContext cAppCtx = new ClassPathXmlApplicationContext("applicationContext.xml");
-        // XmlBeanFactory bf = new XmlBeanFactory(resource);
         ISay bean = cAppCtx.getBean(ISay.class);
         System.err.println(bean.toString());
 
--- SpringInner/src/test/java/org/springinner/AppTest.java
+++ SpringInner/src/test/java/org/springinner/AppTest.java
@@ -1,38 +1,23 @@
 package org.springinner;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
+public class AppTest {
+    @Autowired
+    AbstractApplicationContext cAppCtx;
+
+    @Test
+    public void testName() throws Exception {
+        ISay bean = cAppCtx.getBean(ISay.class);
+        System.err.println(bean.toString());
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
