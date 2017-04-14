c1bbdb4de0df7c135f08f36b926355ea99f50895
songqingqing <songqingqing@foxmail.com>
2017-04-14 18:02:54 +0800

    local commit;

---
+++ CoreJava/src/main/java/org/corejava/bean/hessian/HessianSerializer.java
@@ -0,0 +1,113 @@
+package org.corejava.bean.hessian;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.caucho.hessian.io.HessianInput;
+import com.caucho.hessian.io.HessianOutput;
+
+public class HessianSerializer {
+
+    public static byte[] serialize(Object obj) throws IOException {
+        if (obj == null)
+            throw new NullPointerException();
+
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        HessianOutput ho = new HessianOutput(os);
+        ho.writeObject(obj);
+        return os.toByteArray();
+    }
+
+    public static Object deserialize(byte[] by) throws IOException {
+        if (by == null)
+            throw new NullPointerException();
+
+        ByteArrayInputStream is = new ByteArrayInputStream(by);
+        HessianInput hi = new HessianInput(is);
+        return hi.readObject();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Bean b = new Bean();
+        b.setDt(new Date());
+        b.setI(100);
+        b.setL(1000L);
+        b.setName("test");
+        Map<String, Object> map = new HashMap<>();
+        map.put("dt", new Date());
+        map.put("age", 29);
+        b.setMap(map);
+        byte[] bytes = serialize(b);
+        String s = new String(bytes);
+        System.err.println(s);
+        Object obj = deserialize(bytes);
+        System.err.println(obj);
+
+    }
+    
+
+    static class Bean implements Serializable {
+        /**
+        *
+        */
+        private static final long serialVersionUID = 1L;
+        Date dt;
+        String name;
+        Integer i;
+        Long l;
+        
+        Map<String, Object> map;
+
+        public Date getDt() {
+            return dt;
+        }
+
+        public void setDt(Date dt) {
+            this.dt = dt;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getI() {
+            return i;
+        }
+
+        public void setI(Integer i) {
+            this.i = i;
+        }
+
+        public Long getL() {
+            return l;
+        }
+
+        public void setL(Long l) {
+            this.l = l;
+        }
+
+        public Map<String, Object> getMap() {
+            return map;
+        }
+
+        public void setMap(Map<String, Object> map) {
+            this.map = map;
+        }
+
+        @Override
+        public String toString() {
+            return "Bean [dt=" + dt + ", name=" + name + ", i=" + i + ", l=" + l + ", map=" + map + "]";
+        }
+
+    }
+
+}
--- SpringInner/src/main/java/org/springinner/App.java
+++ SpringInner/src/main/java/org/springinner/App.java
@@ -12,10 +12,10 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.err.println(Number.class.isAssignableFrom(Integer.class));
         // 只能读取main/resources下的文件。
         @SuppressWarnings("resource")
         ClassPathXmlApplicationContext cAppCtx = new ClassPathXmlApplicationContext("applicationContext.xml");
-        ISay bean = cAppCtx.getBean(ISay.class);
         IObj bean1 = cAppCtx.getBean("box", IObj.class);
         System.err.println(bean1.toString());
 
