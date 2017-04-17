a8936f77e8f348696c301c507963aede7bcea473
songqingqing <nkthinker@126.com>
2017-04-17 22:58:37 +0800

    commit

---
+++ SpringInner/src/main/java/org/springinner/aop/TargetBean.java
@@ -0,0 +1,15 @@
+package org.springinner.aop;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TargetBean {
+	static Logger logger = LoggerFactory.getLogger(TargetBean.class);
+
+	private String str = "target String";
+	public void invoke() {
+		logger.debug("invoke Bean method");
+	}
+}
