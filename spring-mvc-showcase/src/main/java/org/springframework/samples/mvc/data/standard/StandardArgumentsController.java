b1ad856d7d7558f4a295e7e758342b3d8fc71232
songqingqing <songqingqing@foxmail.com>
2019-05-28 18:58:20 +0800

    new version for springm mvc

--- spring-mvc-showcase/src/main/java/org/springframework/samples/mvc/data/standard/StandardArgumentsController.java
+++ spring-mvc-showcase/src/main/java/org/springframework/samples/mvc/data/standard/StandardArgumentsController.java
@@ -1,5 +1,6 @@
 package org.springframework.samples.mvc.data.standard;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -7,65 +8,68 @@ import java.io.Reader;
 import java.io.Writer;
 import java.security.Principal;
 import java.util.Locale;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.stereotype.Controller;
+import org.apache.commons.io.FileUtils;
 import org.springframework.util.FileCopyUtils;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class StandardArgumentsController {
 
-	// request related
-	
-	@RequestMapping(value="/data/standard/request", method=RequestMethod.GET)
-	public @ResponseBody String standardRequestArgs(HttpServletRequest request, Principal user, Locale locale) {
-		StringBuilder buffer = new StringBuilder();
-		buffer.append("request = ").append(request).append(", ");
-		buffer.append("userPrincipal = ").append(user).append(", ");
-		buffer.append("requestLocale = ").append(locale);
-		return buffer.toString();
-	}
+    // request related
 
-	@RequestMapping(value="/data/standard/request/reader", method=RequestMethod.POST)
-	public @ResponseBody String requestReader(Reader requestBodyReader) throws IOException {
-		return "Read char request body = " + FileCopyUtils.copyToString(requestBodyReader);
-	}
+    @GetMapping("/data/standard/request")
+    public String standardRequestArgs(HttpServletRequest request, Principal user, Locale locale) {
+        StringBuilder buffer = new StringBuilder();
+        buffer.append("request = ").append(request).append(", ");
+        buffer.append("userPrincipal = ").append(user).append(", ");
+        buffer.append("requestLocale = ").append(locale);
+        return buffer.toString();
+    }
 
-	@RequestMapping(value="/data/standard/request/is", method=RequestMethod.POST)
-	public @ResponseBody String requestReader(InputStream requestBodyIs) throws IOException {
-		return "Read binary request body = " + new String(FileCopyUtils.copyToByteArray(requestBodyIs));
-	}
-	
-	// response related
+    @PostMapping("/data/standard/request/reader")
+    public String requestReader(Reader requestBodyReader) throws IOException {
+        return "Read char request body = " + FileCopyUtils.copyToString(requestBodyReader);
+    }
 
-	@RequestMapping("/data/standard/response")
-	public @ResponseBody String response(HttpServletResponse response) {
-		return "response = " + response;
-	}
+    @PostMapping("/data/standard/request/is")
+    public String requestReader(InputStream requestBodyIs) throws IOException {
+        return "Read binary request body = " + new String(FileCopyUtils.copyToByteArray(requestBodyIs));
+    }
 
-	@RequestMapping("/data/standard/response/writer")
-	public void availableStandardResponseArguments(Writer responseWriter) throws IOException {
-		responseWriter.write("Wrote char response using Writer");
-	}
-	
-	@RequestMapping("/data/standard/response/os")
-	public void availableStandardResponseArguments(OutputStream os) throws IOException {
-		os.write("Wrote binary response using OutputStream".getBytes());
-	}
-	
-	// HttpSession
+    // response related
 
-	@RequestMapping("/data/standard/session")
-	public @ResponseBody String session(HttpSession session) {
-		StringBuilder buffer = new StringBuilder();
-		buffer.append("session=").append(session);
-		return buffer.toString();
-	}
+    @GetMapping("/data/standard/response")
+    public String response(HttpServletResponse response) {
+        return "response = " + response;
+    }
+
+    @GetMapping("/data/standard/response/writer")
+    public void availableStandardResponseArguments(Writer responseWriter) throws IOException {
+        responseWriter.write("Wrote char response using Writer");
+    }
+
+    @GetMapping("/data/standard/response/os")
+    public void availableStandardResponseArguments(OutputStream os) throws IOException {
+        os.write("Wrote binary response using OutputStream".getBytes());
+    }
+
+    @GetMapping("/data/standard/response/pic")
+    public void availableStandardResponseArgumentsForPics(OutputStream os) throws IOException {
+        os.write(FileUtils.readFileToByteArray(new File("C:\\Users\\songq\\Pictures\\wallpics\\1.jpg")));
+    }
+    // HttpSession
+
+    @GetMapping("/data/standard/session")
+    public String session(HttpSession session) {
+        StringBuilder buffer = new StringBuilder();
+        buffer.append("session=").append(session);
+        return buffer.toString();
+    }
 
 }
